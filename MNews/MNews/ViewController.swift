29d0f380c35b8be1a8a3748479d764e0e946e531
chinaiOSSwift <272464282@qq.com>
2016-09-21 16:42:34 +0800

    空文件 -- 测试

---
+++ MNews/MNews/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MNews
+//
+//  Created by qianfeng on 16/9/21.
+//  Copyright © 2016年 qianfeng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = UIColor.whiteColor()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
